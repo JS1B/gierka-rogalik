7c28e9a83064861733bfbdd2f9358783e53cabfd
Piotr Krzemiński <p.krzeminski7@gmail.com>
2024-03-14 14:37:00 +0100

    Add static background

--- game/lib/rogalik_game.dart
+++ game/lib/rogalik_game.dart
@@ -1,17 +1,24 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 import 'components/player_component.dart';
 
 class RogalikGame extends FlameGame with KeyboardEvents {
   late Player player;
+  late SpriteComponent background;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
+    // Load the background
+    background = await loadBackground();
+    add(background);
+
+    // Load the player
     player = Player();
     await add(player);
   }
@@ -39,5 +46,23 @@ class RogalikGame extends FlameGame with KeyboardEvents {
     player.setTargetDirection(direction);
 
     return KeyEventResult.handled;
+  }
+
+  Future<SpriteComponent> loadBackground() async {
+    final sprite = await loadSprite('background.jpg');
+    final size = this.size;
+
+    // Create a paint with a color filter to darken the sprite
+    final paint = Paint()
+      ..colorFilter = ColorFilter.mode(
+        Colors.black.withOpacity(0.6),
+        BlendMode.darken, // Use the darken blend mode
+      );
+
+    return SpriteComponent(
+      sprite: sprite,
+      size: size,
+      paint: paint, // Apply the custom paint
+    );
   }
 }
