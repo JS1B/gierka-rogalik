e7723f63bd7a8a12bed9938fed8def8d628af997
Piotr Krzemiński <p.krzeminski7@gmail.com>
2024-04-09 12:16:40 +0200

    Fix onResize calculation for scenes

--- game/lib/scenes/scene.dart
+++ game/lib/scenes/scene.dart
@@ -23,19 +23,19 @@ abstract class Scene extends Component with HasGameRef<RogalikGame> {
     double drawWidth, drawHeight, dx, dy;
 
     if (imageRatio > canvasRatio) {
-      drawWidth = gameSize.x;
-      drawHeight = gameSize.x / imageRatio;
-      dx = 0;
-      dy = (gameSize.y - drawHeight) / 2;
-    } else {
       drawHeight = gameSize.y;
       drawWidth = gameSize.y * imageRatio;
       dx = (gameSize.x - drawWidth) / 2;
       dy = 0;
+    } else {
+      drawWidth = gameSize.x;
+      drawHeight = gameSize.x / imageRatio;
+      dx = 0;
+      dy = (gameSize.y - drawHeight) / 2;
     }
 
-    this.background!.position = Vector2(dx, dy);
     this.background!.size = Vector2(drawWidth, drawHeight);
+    this.background!.position = Vector2(dx, dy);
   }
 
   void onKeyPress(KeyEvent event, Set<LogicalKeyboardKey> keysPressed);
